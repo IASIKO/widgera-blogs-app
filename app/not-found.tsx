18cf2ca1829f067a5b2f0c9617d2807280cb5bea
IASIKO <iaseshviligi@gmail.com>
2024-10-23 21:47:53 +0400

    feature/add not found resources page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import Button from "@/components/ui/Button";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="text-center flex flex-col gap-4">
+      <h2 className="text-3xl font-semibold">Not Found</h2>
+      <p className="text-xl">Could not find requested resource</p>
+      <Link href="/">
+        <Button type="button">Return Home</Button>
+      </Link>
+    </div>
+  );
+}
