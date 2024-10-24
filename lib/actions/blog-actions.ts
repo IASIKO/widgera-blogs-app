9e527fabf4810d29ab80b20fb9ae88bd7244de90
IASIKO <iaseshviligi@gmail.com>
2024-10-24 17:06:15 +0400

    bugfix/fix route address in create blog action

--- lib/actions/blog-actions.ts
+++ lib/actions/blog-actions.ts
@@ -24,7 +24,7 @@ export async function createBlog(data: BlogFormInputs) {
   const newBlog = { id: Date.now(), ...data };
 
   try {
-    const response = await fetch("http://localhost:3000/api/create-blogs", {
+    const response = await fetch("http://localhost:3000/api/create-blog", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newBlog),
