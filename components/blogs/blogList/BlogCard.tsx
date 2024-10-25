0f286bf62b7a19d2edf1e62187e553c8a74fe0ce
IASIKO <iaseshviligi@gmail.com>
2024-10-25 12:42:52 +0400

    bugfix/fix blog card's link element click issue

--- components/blogs/blogList/BlogCard.tsx
+++ components/blogs/blogList/BlogCard.tsx
@@ -10,7 +10,7 @@ export default function BlogCard({
 }: BlogCardProps) {
   return (
     <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition-transform md:hover:scale-105">
-      <div className="p-6">
+      <div className="p-6 flex flex-col items-start">
         <h2 className="text-xl font-semibold text-indigo-600 mb-2">{title}</h2>
         <p className="text-gray-700 mb-4">{content.slice(0, 30)}...</p>
         <Link href={`/blog/${id}`}>
