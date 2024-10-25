ec931e7e94e7a9ad4f395cc35e2e004697c16f0e
IASIKO <iaseshviligi@gmail.com>
2024-10-25 12:46:48 +0400

    bugfix/fix blog post page's link click issue

--- components/blogs/blogPost/BlogPostContent.tsx
+++ components/blogs/blogPost/BlogPostContent.tsx
@@ -10,7 +10,7 @@ export default function BlogPostContent({
   blog: { title, content },
 }: BlogPostContentProps) {
   return (
-    <div className="max-w-3xl sm:mx-auto p-5 bg-white shadow-md rounded-lg mx-2">
+    <div className="max-w-3xl sm:mx-auto p-5 bg-white shadow-md rounded-lg mx-2 flex flex-col items-start">
       <h1 className="text-4xl font-bold text-indigo-600 mb-4">{title}</h1>
       <div className="prose lg:prose-xl text-gray-800">
         <p>{content}</p>
